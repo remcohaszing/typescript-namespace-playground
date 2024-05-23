bc24ddf93185b0c75cd5928860300562f2438eeb
Remco Haszing <remcohaszing@gmail.com>
2024-05-23 13:24:59 +0200

    Add missing note to a comment

--- script.ts
+++ script.ts
@@ -8,5 +8,7 @@
  * - It’s ok to reference the global `React` namespace for types.
  * - It’s ok to reference the global `React` namespace for values,
  *   even though it will crash at runtime.
+ * - It’s (correctly) not ok to reference the `React` namespace for values if
+ *   `module` is set to `node16` or `nodenext`.
  */
 const element: React.ReactElement = React.createElement()
